005cebe845fdbc4dede835f3ce130adc93c39456
yanqian <yanqian0716@gmail.com>
2021-04-20 17:17:27 +0800

    .

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from sklearn.cluster import KMeans
 from sklearn import metrics
 import numpy as np
 from scipy.spatial.distance import squareform, pdist
+from scipy.special import comb
 # from sklearn import met
 
 from model import Model
@@ -22,6 +23,10 @@ def readImg(imgPath):
     return img
 
 
+def f1_score(model_generated_cluster_labels, target_labels, feature_coll, computed_centroids):
+    return 0
+
+
 def test(model, imgsPath):
     target_labels, imgs_fts = [], []
 
@@ -32,11 +37,9 @@ def test(model, imgsPath):
         imgs_fts.append(img_ft)
 
     imgs_fts = np.vstack(imgs_fts).astype(np.float32)
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(imgs_fts)
+    kmeans = KMeans(n_clusters=config.N_CLASSES, random_state=0).fit(imgs_fts)
     model_generated_cluster_labels = kmeans.labels_
     computed_centroids = kmeans.cluster_centers_
-    print(model_generated_cluster_labels)
-    print(computed_centroids.shape)
 
     NMI = metrics.cluster.normalized_mutual_info_score(
         model_generated_cluster_labels.reshape(-1),
@@ -44,12 +47,15 @@ def test(model, imgsPath):
     )
 
     k_closest_points = squareform(pdist(imgs_fts)).argsort(1)[:, :int(np.max(config.K_VALS)+1)]
-    k_closest_classes = target_labels.reshape(-1)
+    k_closest_classes = target_labels.reshape(-1)[k_closest_points[:, 1:]]
 
     recall_all_k = []
     for k in config.K_VALS:
-        recall_all_k = np.sum([1 for ])
+        recall_at_k = np.sum([1 for target, recalled_pred in zip(target_labels, k_closest_classes)
+                               if target in recalled_pred[:k]]) / len(target_labels)
+        recall_all_k.append(recall_at_k)
 
+    F1 = f1_score(model_generated_cluster_labels, target_labels, imgs_fts, computed_centroids)
     return 0
 
 
