8d374c0ebb38dc525d1432e8f466708fbddf4812
yanqian <yanqian0716@gmail.com>
2021-04-22 14:11:26 +0800

    .

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ START_EPOCH = 0
 LOG_EPOCH = 10
 SAVE_PATH = './weights/W'
 CONTINUE = True
-CONTINUE_PATH = '../input/weights/weights/'
+CONTINUE_PATH = '../input/weightss/weights'
 
 
 # optimizer
